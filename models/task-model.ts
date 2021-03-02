5e124e890df25ddf982999d1f1ec9e1d50cb2418
Yuta OBARA <obara@NguyennoMacBook-Pro.local>
2021-03-02 14:38:35 +0900

    rm cache

--- models/task-model.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const taskSchema = new mongoose.Schema({
-    description: {type: String, required: true},
-}, {
-    timestamps: true,
-});
-
-export const Task = mongoose.model("Task", taskSchema)
