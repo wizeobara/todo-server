5b96ce8e723694c2473553e802ac4668daf7ac0e
Yuta OBARA <obara@NguyennoMacBook-Pro.local>
2021-03-18 16:17:31 +0900

    added user to schema

--- models/task-model.ts
+++ models/task-model.ts
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 export const taskSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
     completed: { type: Boolean, required: true },
+    title: { type: String, required: true },
+    user: { type: String, required: true },
   },
   {
     timestamps: true,
   }
 );
+
+taskSchema.add({ child: [taskSchema] });
 
 export const Task = mongoose.model("Task", taskSchema);
